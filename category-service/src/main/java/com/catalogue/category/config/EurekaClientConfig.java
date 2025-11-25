89af6a770b0cb938bea9ffe93756f377f020d18d
Houssaine-X <154687522+Houssaine-X@users.noreply.github.com>
2025-11-25 11:23:41 +0100

    ConfigServer

---
+++ category-service/src/main/java/com/catalogue/category/config/EurekaClientConfig.java
@@ -0,0 +1,11 @@
+package com.catalogue.category.config;
+import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class EurekaClientConfig {
+    @Bean
+    public Jersey3TransportClientFactories jersey3TransportClientFactories() {
+        return Jersey3TransportClientFactories.getInstance();
+    }
+}
