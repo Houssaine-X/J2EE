c9e61c7f604fa7baa4a69fb0373192aeb1f329b2
Ayoub MHANDI <Ayoub.Mhandi@emsi-edu.ma>
2025-12-15 17:11:03 +0100

    Add 'Add to Cart' functionality to product cards

--- catalogue-frontend/src/app/features/products/product-list/product-list.component.ts
+++ catalogue-frontend/src/app/features/products/product-list/product-list.component.ts
@@ -9,11 +9,14 @@ import { InputTextModule } from 'primeng/inputtext';
 import { TagModule } from 'primeng/tag';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 import { Product } from '../../../core/models/product.model';
 import { AppState } from '../../../store/app.state';
 import * as ProductActions from '../../../store/product/product.actions';
 import * as ProductSelectors from '../../../store/product/product.selectors';
+import * as CartActions from '../../../store/cart/cart.actions';
 
 @Component({
   selector: 'app-product-list',
@@ -26,8 +29,10 @@ import * as ProductSelectors from '../../../store/product/product.selectors';
     ButtonModule,
     InputTextModule,
     TagModule,
-    ProgressSpinnerModule
+    ProgressSpinnerModule,
+    ToastModule
   ],
+  providers: [MessageService],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
@@ -38,7 +43,10 @@ export class ProductListComponent implements OnInit {
 
   searchKeyword = '';
 
-  constructor(private store: Store<AppState>) {
+  constructor(
+    private store: Store<AppState>,
+    private messageService: MessageService
+  ) {
     this.products$ = this.store.select(ProductSelectors.selectAllProducts);
     this.loading$ = this.store.select(ProductSelectors.selectProductsLoading);
     this.error$ = this.store.select(ProductSelectors.selectProductsError);
@@ -58,6 +66,16 @@ export class ProductListComponent implements OnInit {
     } else {
       this.loadProducts();
     }
+  }
+
+  addToCart(product: Product): void {
+    this.store.dispatch(CartActions.addToCart({ product, quantity: 1 }));
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Added to Cart',
+      detail: `${product.nom} has been added to your cart`,
+      life: 3000
+    });
   }
 
   getStockSeverity(product: Product): 'success' | 'secondary' | 'info' | 'warn' | 'danger' | 'contrast' | null {
