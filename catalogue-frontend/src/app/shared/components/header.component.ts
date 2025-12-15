d3c77965757c2e89018aa4cc2f74d42310ef8623
Ayoub MHANDI <Ayoub.Mhandi@emsi-edu.ma>
2025-12-15 17:10:02 +0100

    Update header with cart badge and improved navbar styling

--- catalogue-frontend/src/app/shared/components/header.component.ts
+++ catalogue-frontend/src/app/shared/components/header.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { MenubarModule } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 import { MenuItem } from 'primeng/api';
+import { AppState } from '../../store/app.state';
+import * as CartSelectors from '../../store/cart/cart.selectors';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, MenubarModule, BadgeModule, ButtonModule],
+  imports: [CommonModule, RouterModule, MenubarModule, BadgeModule, ButtonModule, TooltipModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  cartItemCount$: Observable<number>;
+
   menuItems: MenuItem[] = [
     {
       label: 'Accueil',
@@ -41,5 +48,9 @@ export class HeaderComponent {
       routerLink: '/orders'
     }
   ];
+
+  constructor(private store: Store<AppState>) {
+    this.cartItemCount$ = this.store.select(CartSelectors.selectCartTotalItems);
+  }
 }
 
